bd68f959d5c627c2a258d95240daf2685d97a024
Pankaj <mepankaj@live.in>
2017-01-30 21:52:17 -0500

    Added implementation of the method ninetyNineBottlesOfBeer

--- BeerSong.java
+++ BeerSong.java
@@ -6,7 +6,22 @@ public class BeerSong {
 	}
 
 	void ninetyNineBottlesOfBeer() {
+        int beerNum = 99;
+		String word = "bottles";
+		while (beerNum > 0) {
+			System.out.println(beerNum + " " + word + " of beer on the wall," + beerNum + " " + word + " of beer.");
+			System.out.println("Take one down, pass it around.");
 
+			beerNum = beerNum - 1;
+			if (beerNum == 1) {
+				word = "bottle";
+			}
+			if (beerNum > 0) {
+				System.out.println(beerNum + " " + word + " of beer on the wall");
+				System.out.println("-----------------------------");
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+			}
+		}
 	}
-
 }
